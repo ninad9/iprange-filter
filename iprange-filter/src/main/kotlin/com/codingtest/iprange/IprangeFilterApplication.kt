ebf4f8d906950b00a8dd107cdced386ad58d6894
ninad9 <78981903+ninad9@users.noreply.github.com>
2025-06-27 20:03:24 +0200

    Adding iprange-filter project

---
+++ iprange-filter/src/main/kotlin/com/codingtest/iprange/IprangeFilterApplication.kt
@@ -0,0 +1,11 @@
+package com.codingtest.iprange
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IprangeFilterApplication
+
+fun main(args: Array<String>) {
+	runApplication<IprangeFilterApplication>(*args)
+}
---
+++ iprange-filter/src/test/kotlin/com/codingtest/iprange/IprangeFilterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codingtest.iprange
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IprangeFilterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
